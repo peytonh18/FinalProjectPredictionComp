52ce41ee670d7396fa3404cbf3c1d3aa5ac83348
Sam McFarland <smcfarl75@gmail.com>
2022-04-30 17:55:52 -0400

    Added more models

--- FinalProject.R
+++ FinalProject.R
@@ -40,6 +40,38 @@ fit<-TRAIN %>%
     
     arima=ARIMA(ï..credit_in_millions),
     
-    lm=TSLM(ï..credit_in_millions))
+    lm=TSLM(ï..credit_in_millions),
+    
+    rw = RW(ï..credit_in_millions ~drift()),
+    
+    Naive = NAIVE(ï..credit_in_millions),
+    
+    SN = SNAIVE(ï..credit_in_millions),
+    
+    ETS2 = ETS(ï..credit_in_millions~ error("A") + trend("N") + season("N")),
+    
+    ETS3 = ETS(ï..credit_in_millions~ error("A") + trend("A") + season("N")),
+    
+    ETS4 = ETS(ï..credit_in_millions~ error("A") + trend("A") + season("A")),
+    
+    ETS5 = ETS(ï..credit_in_millions ~ error("A") + trend("Ad", phi = 0.9) + season("N")),
+    
+    ETS6 = ETS(ï..credit_in_millions~ error("M") + trend("A") + season("M")),
+    
+    ETS7 = ETS(ï..credit_in_millions~ error("M") + trend("Ad") + season("M")),
+    
+    arima210 = ARIMA(ï..credit_in_millions ~ pdq(2,1,0)),
+    
+    arima013 = ARIMA(ï..credit_in_millions ~ pdq(0,1,3)),
+    
+    arima012011 = ARIMA(ï..credit_in_millions ~ pdq(0,1,2) + PDQ(0,1,1)),
+    
+    arima210011 = ARIMA(ï..credit_in_millions ~ pdq(2,1,0) + PDQ(0,1,1)),
+    
+    stochastic = ARIMA(ï..credit_in_millions ~ pdq(d = 1)),
+    
+    CROSTON = CROSTON(ï..credit_in_millions),
+    
+    lm2 = TSLM(ï..credit_in_millions ~ trend()))
 fit %>% forecast(h=6) %>% accuracy(TRAIN) %>% arrange(RMSE)
 
